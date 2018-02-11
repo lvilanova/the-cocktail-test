7197badc6f1727b91add58936a242e0e63559ead
Luis Vilanova <luisvilanovarodriguez@gmail.com>
2018-02-11 23:55:42 +0100

    ejecution order displaying tabs

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     //here manage displays tabs, tab1 is always display by default
-    document.getElementById('tab1').style.display = "block";
-
     var i, content;
     content = document.getElementsByClassName("content");
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
-
+    document.getElementById('tab1').style.display = "block";
 });
