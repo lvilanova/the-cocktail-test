c4d9c922160828ac36418549fa3c9a30b41b3aa0
Luis Vilanova <lvilanova@Luis-Vilanova-2.local>
2018-02-12 17:34:55 +0100

    javascript code for managing views

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,5 +5,33 @@ $(document).ready(function () {
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
+
     document.getElementById('tab1').style.display = "block";
+
+    function navTab(evt, numberTab) {
+        var i, content, tablinks;
+        content = document.getElementsByClassName("js-content");
+        for (i = 0; i < content.length; i++) {
+            content[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(numberTab).style.display = "block";
+        evt.currentTarget.className += " active";
+
+    }
+
+    var el = document.getElementsByClassName("tablinks");
+    el[0].onclick = function (event) {
+        navTab(event, "tab1");
+    };
+    el[1].onclick = function (event) {
+        navTab(event, "tab2");
+    };
+
+
+
+
 });
