5ea343a5c8a41ffd8471f636c194752e8ebc181d
Luis Vilanova <lvilanova@Luis-Vilanova-2.local>
2018-02-12 16:17:04 +0100

    manage transitions with js

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     //here manage displays tabs, tab1 is always display by default
     var i, content;
-    content = document.getElementsByClassName("content");
+    content = document.getElementsByClassName("js-content");
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
-    document.getElementById('tab2').style.display = "block";
+    document.getElementById('tab1').style.display = "block";
 });
